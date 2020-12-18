cd4442d04a2a7167ad5f839402c0ff0d234769ca
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-12-18 11:06:06 -0500

    refactor class

--- src/com/fabrizio/GoFishHand.java
+++ src/com/fabrizio/GoFishHand.java
@@ -46,4 +46,10 @@ public class GoFishHand {
         }
         return false;
         }
+
+        public void displayHand(){
+        for(var card : cards){
+            System.out.println(card.toString() + " ");
+        }
+        }
 }
--- src/com/fabrizio/Table.java
+++ src/com/fabrizio/Table.java
@@ -1,11 +1,13 @@
 package com.fabrizio;
 
 public class Table {
-    private GoFishHand player;
+    private GoFishHand player = new GoFishHand(new Player());
     private GoFishHand opponent;
     private Standard deck = new Standard();
 
     public Table(){}
+
+
 
 
 }
